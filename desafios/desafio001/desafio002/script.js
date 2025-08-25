df75e4648027c1cd4e6332e15e44dbbe1a67009b
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-25 03:19:56 -0300

    Otimizando os códigos

--- desafios/desafio001/desafio002/script.js
+++ desafios/desafio001/desafio002/script.js
@@ -17,11 +17,11 @@ function verificar() {
             document.body.style.background = '#3102ffff'
             if (idade <= 10) {
                 img.setAttribute('src', 'babyman.jpg')
-            } else if (idade > 10 && idade < 31) {
+            } else if (idade < 31) {
                 img.setAttribute('src', 'jovemman.jpg')
-            } else if (idade > 30 && idade < 61) {
+            } else if (idade < 61) {
                 img.setAttribute('src', '40man.jpg')
-            } else if (idade > 60) {
+            } else {
                 img.setAttribute('src', 'idoso.jpg')
              }
         } else {
@@ -29,16 +29,16 @@ function verificar() {
             document.body.style.background = '#ff44efff'
             if (idade <= 10) {
                 img.setAttribute('src', 'bebemulher.jpg')
-            } else if (idade > 10 && idade < 31) {
+            } else if (idade < 31) {
                 img.setAttribute('src', 'jovemmulher.jpg')
-            } else if (idade > 30 && idade < 61) {
+            } else if (idade < 61) {
                 img.setAttribute('src', '40mulher.jpg')
-            } else if (idade > 60) {
+            } else {
                 img.setAttribute('src', 'idosa.jpg')
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos.`
         res.appendChild(img)
     }
 }
