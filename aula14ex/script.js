6c46d6995a0c7f55038861bf7505004279a5dd9f
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-28 02:41:14 -0300

    Adicionando no script

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -2,11 +2,19 @@ function verificar() {
     var inicio = document.getElementById("inicio")
     var fim = document.getElementById("fim")
     var passos = document.getElementById("passos")
-    if (inicio < fim || inicio < passos) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } else {
-        for (inicio;inicio < fim;inicio += passos) {
-            
+    var res = document.getElementById("res")
+    var i = Number(inicio.value)
+    var f = Number(fim.value)
+    var p = Number(passos.value)
+    if (i <= 0 || f <= 0 || p <= 0){
+        alert("FALTAM DADOS!")
+    } 
+    if (i < f || i < p) {
+        alert("[ERRO] Verifique os dados e tente novamente!")
+    }
+    else {
+        for (var c=i;c <= f;c += p) {
+            res.innerHTML += `contando`
         }
     }
 }
