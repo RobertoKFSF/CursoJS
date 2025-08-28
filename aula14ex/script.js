4fe443e2c5ddf0a39688e567f403144e307a3d02
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-28 02:22:29 -0300

    Adicionando o script

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -1,3 +1,12 @@
-function carregar() {
-    var inicio = document.getElementById
+function verificar() {
+    var inicio = document.getElementById("inicio")
+    var fim = document.getElementById("fim")
+    var passos = document.getElementById("passos")
+    if (inicio < fim || inicio < passos) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        for (inicio;inicio < fim;inicio += passos) {
+            
+        }
+    }
 }
