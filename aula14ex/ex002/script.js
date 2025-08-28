72194b29972531d328ad9f63a480dffa9f692e61
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-28 03:44:45 -0300

    Exercicio feito

--- aula14ex/ex002/script.js
+++ aula14ex/ex002/script.js
@@ -6,30 +6,13 @@ function tabuada() {
     }
     else {
         var n = Number(num.value)
+        var t = Number(tab.value)
         let c = 1
         while (c <= 10) {
             let item = document.createElement('option')
-            item.text = `${} x ${}`
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
         }
-    }
-    var t = Number(tab.value)
-    if (p <= 0){
-        p = 1
-    } 
-    if (p > f) {
-        alert("[ERRO] Verifique os dados e tente novamente!")
-    }
-    else if (i >= f){
-        for (var c = i;f<=c;c -= p) {
-            res.innerHTML += `${c} \u{1F449}`
-        }
-        res.innerHTML += `\u{1F3C1}`
-    }
-    else {
-        res.innerHTML = ("Contando: ")
-        for (var c=i;c <= f;c += p) {
-            res.innerHTML += `${c} \u{1F449}`
-        }
-        res.innerHTML += `\u{1F3C1}`
     }
 }
