0bea1f8ec720529c22e957beca1a3408fbd1c593
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-30 02:19:11 -0300

    Advinhando a posição do numero pesquisando por ele

--- aula015/script.js
+++ aula015/script.js
@@ -16,3 +16,7 @@ for(let pos in num2){
     console.log(`O valor na posição ${pos}º É: ${num2[pos]}`)
 }
 console.log(`FIM DE PROGRAMA!`)
+
+let num3 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let res = prompt("Digite um número de 0 a 9: ");
+console.log(`O valor ${res} ta na posição ${num3.indexOf(res)}`)
