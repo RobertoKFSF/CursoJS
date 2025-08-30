c41ce405517b0b766bee1bf2b9e5bc50de97cb4c
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-30 03:21:16 -0300

    Testando programa

--- aula016015ex/ex002/script.js
+++ aula016015ex/ex002/script.js
@@ -1,10 +1,10 @@
-var num = document.getElementById("num")
-var add = document.querySelector("add")
-let fim = document.querySelector("fim")
+var num = document.getElementById("input#num")
+var lista = document.querySelector("select#add")
+let res = document.querySelector("div#res")
 let valores = []
 
 function isNumero(n) {
-        if (isNumero(num.value) < 0 && isNumero(num.value > 100)) {
+        if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
@@ -19,13 +19,19 @@ function isLista(n, l) {
     }
 }
 function adicionar() {
-    if (num >= 0) {
-        valores.push(num.value)
-        var n = Number(num.value)
-        let c = 1
-        let item = document.createElement('option')
-        item.text = `O valor adicionado foi ${num}`
-        add.appendChild(item)
-        c++
+    if (isNumero(num.value) && !isLista(num.value, valores)) {
+    
+    } else {
+        alert ("Valor invalido ou ja encontra-se na lista.")
     }
 }
+
+
+if (num >= 0) {
+    valores.push(num.value)
+    var n = Number(num.value)
+    let c = 1
+    let item = document.createElement('option')
+    item.text = `O valor adicionado foi ${num}`
+    add.appendChild(item)
+    c++ }
