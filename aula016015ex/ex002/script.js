cae22c95a104463b75eab0405b37d797338551f9
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-30 03:58:25 -0300

    Programa finalizado!

--- aula016015ex/ex002/script.js
+++ aula016015ex/ex002/script.js
@@ -29,7 +29,28 @@ function adicionar() {
     }
 }
 function finalizar() {
-    res.innerHTML `Ao todo, temos ${num.value.length + 1} cadastrados.`
+    if (valores.length == 0) {
+        alert ('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if(maior < valores[pos])
+                maior = valores[pos]
+            if(menor > valores[pos])
+                menor = valores[pos]
+    }
+    media = soma / tot
+    res.innerHTML += `<p>Ao todo, temos ${tot} cadastrados.</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+    res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    }
 }
 
 
