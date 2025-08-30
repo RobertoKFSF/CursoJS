3b3cb8e7f589d99b9e786cc42ec0c422930405ac
RobertoKFSF <224346189+RobertoKFSF@users.noreply.github.com>
2025-08-30 02:15:27 -0300

    Criando de um jeito mais simples

--- aula015/script.js
+++ aula015/script.js
@@ -6,8 +6,13 @@ console.log(`${num.sort()}`)
 console.log(`Nosso vetor é o ${num}`)
 
 let num1 = [2, 4, 6, 1, 5, 9, 10, 12, 53, 32]
-let cont = 0
-for (0; cont < num1.length; cont++) {
-    console.log(`${num1[cont]}`)
+for (let cont = 0; cont < num1.length; cont++) {
+    console.log(`O valor na posição ${cont}º É: ${num1[cont]}`)
+}
+console.log(`FIM DE PROGRAMA!`)
+
+let num2 = [2, 4, 6, 1, 5, 9, 10, 12, 53, 32]
+for(let pos in num2){
+    console.log(`O valor na posição ${pos}º É: ${num2[pos]}`)
 }
 console.log(`FIM DE PROGRAMA!`)
